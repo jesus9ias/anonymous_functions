297c7a2a4509d0a1f5d115a3ee44242632c55062
Jesús Escamilla <jesusescamilla@MacBook-Pro-de-Jesus.local>
2015-08-14 20:46:30 -0500

    Ini repository

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+function run_function($function){
+	
+	$params = func_get_args();
+	unset( $params[0] );
+
+	if(($count = count($params)) > 0){
+		$args = '$a';
+		$inc = 'b';
+
+		
+		for($x = 1; $x < $count; $x++){
+			$args .= ', $' . $inc;
+			$inc++;
+		}
+
+		$params = "'" . implode('\', \'', $params) . "'";
+
+		
+		$eval = '$return = $function(' . $params . ');';
+		eval($eval);
+
+		return $return;
+	}else{
+		return false;
+	}
+}
+
+echo run_function(function($a,$b){return $a+$b;},5,7);
+
+?>
